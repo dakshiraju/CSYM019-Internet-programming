c38e820f22c3848b4561d61bb63e3aadecdf11ff
shail <shailud1946@gmail.com>
2024-03-08 12:14:26 +0000

    feat: upload to github

---
+++ week 4/exercise3.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var element=document.getElementById("id1");
+    element.firstChild.nodeValue="New Heading";
+
+    var element=document.getElementById("id2");
+    element2.firstchild.nodeValue="New content in paragraph";
+}
+
+document.addEventListener('DOMContentLoaded',myfunction);
