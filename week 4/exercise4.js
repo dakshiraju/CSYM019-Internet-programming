fcaaca9d530f58f9d70b3b8e7bc1ce07410fea20
shail <shailud1946@gmail.com>
2024-03-10 11:53:09 +0000

    Changed DOMContentLoaded to click

--- week 4/exercise4.js
+++ week 4/exercise4.js
@@ -2,7 +2,7 @@ function myFunction(){
     var element=document.getElementById("id1");
     element.firstChild.nodeValue="New Heading";
     var element2=document.getElementById("id2");
-    element2.firstChild.nodeValue="New content in paragraph";
+    element2.firstChild.nodeValue="New contents in paragraph";
 }
 
 document.addEventListener('click',myFunction);
