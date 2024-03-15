ee6ebac5bac1f480cc067a951f0fd792a582f19a
shail <shailud1946@gmail.com>
2024-03-15 13:04:46 +0000

    resources for exercises

---
+++ week 5/exercise1.js
@@ -0,0 +1,12 @@
+
+function myClickEvent()
+{
+    alert('The button was pressed');
+}
+
+function myLoadFunction(){
+    var element=document.getElementById('circle');
+    element.addEventListener('click',myClickEvent);
+}
+
+document.addEventListener('DoMContentLoaded',myLoadFunction);
